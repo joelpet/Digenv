dde7ee600e81acc8800c37c8c363d978ff293062
Joel Pettersson <petterssonjoel@gmail.com>
2011-04-26 19:15:06 +0200

    Add small environment variables test program.

--- code/envtestprint.c
+++ code/envtestprint.c
@@ -1,11 +1,27 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main (int argc, char **argv, char **envp) {
 
+    pid_t childpid;
     int i;
+
+    envp[0] = "parent";
+
     for (i = 0; envp[i] != NULL; ++i) {
         printf("%2d: %s\n", i , envp[i]);
     }
+
+    childpid = fork();
+
+    if (0 == childpid) {
+        envp[0] = "child";
+        exit(0);
+    } 
+
+
+    printf("%s\n", envp[0]);
 
 }
